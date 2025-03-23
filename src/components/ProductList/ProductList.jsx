5eb639c13c9975ab743b4c57404b097ec4d9eb28
Grigory Krutov <grukrutov@192.168.1.6>
2025-03-23 20:27:09 +0300

    bug fix ProductItem

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -41,7 +41,7 @@ const ProductList = () => {
   };
   return (
     <div className={'list'}>
-      {ProductList.map((item) => (
+      {products.map((item) => (
         <ProductItem product={item} onAdd={onAdd} className={'item'} />
       ))}
     </div>
