1644836398f6cf39faca5cb18280a0e856c54b79
Grigory Krutov <grukrutov@192.168.1.6>
2025-03-23 20:52:23 +0300

    bug fix mainButton

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -36,7 +36,7 @@ const ProductList = () => {
       tg.MainButton.hide();
     } else {
       tg.MainButton.show();
-      tg.MainButton.setParams({text: `Купить ${getTotalPrice}`})
+      tg.MainButton.setParams({text: `Купить ${getTotalPrice(newItems)}`})
     }
   };
   return (
