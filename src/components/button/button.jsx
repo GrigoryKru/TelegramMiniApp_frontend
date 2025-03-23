593f6f623c475f2aaa67be88e90e7cb707dc02a6
Grigory Krutov <grukrutov@192.168.1.6>
2025-03-23 17:54:27 +0300

    bug fix with button

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './button.css';
 
 const button = (props) => {
-  return <button {...props} ClassName={'button' + props.ClassName} />;
+  return <button {...props} сlassName={'button' + props.ClassName} />;
 };
 
 export default button;
