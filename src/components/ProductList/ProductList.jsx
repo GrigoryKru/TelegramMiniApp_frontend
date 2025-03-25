6dca4be41c9164ccce8d368b769fa55228f32e46
Grigory Krutov <grukrutov@192.168.1.6>
2025-03-25 21:15:06 +0300

    add back part in front

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,24 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import './ProductList.css';
 import ProductItem from '../ProductItem/ProductItem';
 import { useTelegram } from '../hooks/use-telegram';
 
 const products = [
-  {id: '1', title: 'Джинсы', price: 5000, description: 'Нормик'},
-  {id: '2', title: 'Джинсы2', price: 2344, description: 'Нормик'},
-  {id: '3', title: 'Джинсы3', price: 1234, description: 'Нормик'},
-  {id: '4', title: 'Джинсы4', price: 2345, description: 'Нормик'},
-]
+  { id: '1', title: 'Джинсы', price: 5000, description: 'Нормик' },
+  { id: '2', title: 'Джинсы2', price: 2344, description: 'Нормик' },
+  { id: '3', title: 'Джинсы3', price: 1234, description: 'Нормик' },
+  { id: '4', title: 'Джинсы4', price: 2345, description: 'Нормик' },
+];
 
 const getTotalPrice = (items = []) => {
-  return items.reduce((acc,item) => {
-    return acc += item.price
-  }, 0)
-}
+  return items.reduce((acc, item) => {
+    return (acc += item.price);
+  }, 0);
+};
 
 const ProductList = () => {
   const [addedItems, setAddedItems] = useState([]);
-  const { tg } = useTelegram();
+  const { tg, queryId } = useTelegram();
+
+  const onSendData = useCallback(() => {
+    const data = {
+      products: addedItems,
+      totalPrice: getTotalPrice(addedItems),
+      queryId,
+    };
+    fetch('http://localhost:8000', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+  }, [addedItems]);
+
+  useEffect(() => {
+    tg.onEvent('mainButtonClicked', onSendData);
+    return () => {
+      tg.offEvent('mainButtonClicked', onSendData);
+    };
+  }, [onSendData, tg]);
 
   const onAdd = (product) => {
     const alreadyAdded = addedItems.find((item) => item.id === product.id);
@@ -36,13 +58,21 @@ const ProductList = () => {
       tg.MainButton.hide();
     } else {
       tg.MainButton.show();
-      tg.MainButton.setParams({text: `Купить ${getTotalPrice(newItems)}`})
+      tg.MainButton.setParams({ 
+        text: `Купить ${getTotalPrice(newItems)}` 
+      });
     }
   };
+
   return (
     <div className={'list'}>
       {products.map((item) => (
-        <ProductItem product={item} onAdd={onAdd} className={'item'} />
+        <ProductItem 
+          key={item.id}
+          product={item} 
+          onAdd={onAdd} 
+          className={'item'} 
+        />
       ))}
     </div>
   );
